49ff6aa02e2499bda7047852070f66865d0abc27
Florian POURTUGAU <florian.pourtugau@hotmail.fr>
2018-01-22 15:37:49 +0100

    Removed unused import

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import sys
 import unittest
 from configparser import NoOptionError, NoSectionError
 from configparser_extended import ExtendedConfigParser, SectionProxyExtended
